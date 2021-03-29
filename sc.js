00e9b7120f73a66a951fd299799f09b7d34d2467
dubanyesid <dubanyesidsb@ufps.edu.co>
2021-03-28 21:08:32 -0500

    Trabajo Terminado

---
+++ sc.js
@@ -0,0 +1,7 @@
+$.getJSON('partidos.json', function(datos) {
+
+    $("#contenido").html('');
+    $.each(datos["partidos"], function(idx, partido) {
+        $("#contenido").append("<tr>" + "<td>" + partido["fecha"] + "</td><td>" + partido["equipo1"] + "</td><td>" + partido.equipo2 + "</td>" + "</tr>");
+    })
+});
